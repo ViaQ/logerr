7d886642f7cc0105513a181c88e9b397c9428b10
Robert Jacob <rojacob@redhat.com>
2022-03-29 13:45:49 +0200

    Use same golangci-lint as on CI

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -17,9 +17,9 @@ GO     ?= $(shell which go)
 #	@echo "Running golangci-lint"
 #	@$(GOLANGCI_LINT) <flags/args..>
 #
-GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.31.0
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.32.2
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/golangci-lint-v1.31.0"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.31.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.32.2"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.32.2 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
